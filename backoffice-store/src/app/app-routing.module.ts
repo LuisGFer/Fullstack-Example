dff9201f7337647b7aa47b3bb811d583436ee1d7
Luis Fernandez <luisg.fernandez.rodriguez@gmail.com>
2022-05-18 07:05:02 +0200

    Formulario reactivo

--- backoffice-store/src/app/app-routing.module.ts
+++ backoffice-store/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CategoryListComponent } from './entities/category/category-list/category-list.component';
 import { ItemFormComponent } from './entities/item/item-form/item-form.component';
 import { ItemListComponent } from './entities/item/item-list/item-list.component';
+import { ItemReactiveFormComponent } from './entities/item/item-reactive-form/item-reactive-form.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path: 'categories', component: CategoryListComponent },
   { path: 'items', component: ItemListComponent },
   { path: 'categories/:categoryId/items', component: ItemListComponent },
-  { path: 'items/:itemId', component: ItemFormComponent}
+  { path: 'items/:itemId', component: ItemFormComponent},
+  { path: 'items/reactive/:itemId', component: ItemReactiveFormComponent}
 ];
 
 @NgModule({
